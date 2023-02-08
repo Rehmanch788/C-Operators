6d797a7be3873944bc6192192096b096b171aec1
Rehman ch <124032207+Rehmanch788@users.noreply.github.com>
2023-02-08 04:53:57 -0800

    Add files via upload
    Programs

---
+++ Program1.cpp
@@ -0,0 +1,11 @@
+// displays your favorite poem
+#include <iostream>      //preprocessive directive
+using namespace std;
+int main()   //main function
+{
+       cout<<"Nash:"<<endl   //using statement to end the line
+              <<"Candy is dandy,"<<endl
+              <<"But liquor is quicker."<<endl;
+              return 0;
+              
+}
---
+++ program2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>   //preprocessive directive 
+using namespace std;
+int main()    //main function
+{
+       int var = 10;
+       cout << var << endl;      // var is 10
+       var *= 2;                 // var becomes 20
+       cout << var-- << endl;    // displays var, then decrements it
+       cout << var << endl;      // var is 19
+       return 0;   //end the function
+
+}
